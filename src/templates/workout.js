e6aeaa2171b85915998eb6d885e82a998064b819
Guayo Mena <guayo.mena@gmail.com>
2020-01-31 10:21:38 -0600

    Align buttons

--- src/templates/workout.js
+++ src/templates/workout.js
@@ -5,22 +5,34 @@ import { Row } from "../components/summary"
 export default ({ data }) => {
   const { markdownRemark } = data
   const { title, excercises } = markdownRemark.frontmatter
-  // const [excercise, setExcercise] = useState(0)
-  const [set, setSet] = useState(0)
+  const [excercise, setExcercise] = useState(0)
+  // const [set, setSet] = useState(0)
+
+  const Move = (excercise, amount) => {
+    setExcercise(excercise + amount)
+  }
 
   return (
     <Layout>
       <h1>{title}</h1>
-      <h2>Set: {set + 1}</h2>
+      <h2>Excercise: {excercise + 1}</h2>
       <div>
-        {excercises.map(ex => <Row {...ex} />)}
+        {excercises.map(ex => (
+          <Row {...ex} />
+        ))}
       </div>
-      <div style={{ display: "flex", justifyContent: "space-between" }}></div>
-      <div></div>
-      {set > 0 && <button onClick={() => setSet(set - 1)}>Previous Set</button>}
-      {set >= 0 && set < 3 && (
-        <button onClick={() => setSet(set + 1)}>Next Set</button>
-      )}
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <div>
+          {excercise > 0 && (
+            <button onClick={() => Move(excercise, -1)}>Previous</button>
+          )}
+        </div>
+        <div>
+          {excercise >= 0 && excercise < 3 && (
+            <button onClick={() => Move(excercise, 1)}>Next</button>
+          )}
+        </div>
+      </div>
     </Layout>
   )
 }
